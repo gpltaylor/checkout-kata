cd41b254ac2ac748e1ccae913227f2f33e68237b
Garry Taylor <garrytaylor@fluentmoney.co.uk>
2024-03-04 20:03:59 +0000

    State and Data Thinking about where to load state. How to calculate this using ADT or a clean method.. This is a basket so we are talking 100s not bil of data points
    Signed-off-by: Garry Taylor <garrytaylor@fluentmoney.co.uk>

--- checkout.go
+++ checkout.go
@@ -7,10 +7,23 @@ type ICheckout interface {
 
 type Checkout struct {
 	totalPrice int
+	basket     map[string]int
+	ItemPrices map[string]ItemPrice
+}
+
+type ItemPrice struct {
+	sku           string
+	price         int
+	multiPrice    int
+	multiPriceQty int
 }
 
 func (c *Checkout) Scan(item string) {
 	// Add scanned item to a Key Value store (Map)
+	if c.basket == nil {
+		c.basket = make(map[string]int)
+	}
+	c.basket[item]++
 }
 
 func (c *Checkout) GetTotalPrice() int {
@@ -19,5 +32,13 @@ func (c *Checkout) GetTotalPrice() int {
 }
 
 func NewCheckout() ICheckout {
-	return &Checkout{}
+	return &Checkout{
+		// TODO: Load from Redis or something (Dapr state store?)
+		ItemPrices: map[string]ItemPrice{
+			"A": {"A", 50, 130, 3},
+			"B": {"B", 30, 45, 2},
+			"C": {"C", 20, 0, 0},
+			"D": {"D", 15, 0, 0},
+		},
+	}
 }
